d65c46de966ceb36e5fedc0a8e539de8be6c0dfe
Feross Aboukhadijeh <feross@feross.org>
2016-03-08 16:21:10 -0800

    huge refactor to make removing the .drag class more reliable

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ var flatten = require('flatten')
 var parallel = require('run-parallel')
 
 function dragDrop (elem, listeners) {
-  if (typeof elem === 'string') elem = window.document.querySelector(elem)
-  if (typeof listeners === 'function') listeners = { onDrop: listeners }
+  if (typeof elem === 'string') {
+    elem = window.document.querySelector(elem)
+  }
 
-  var onDragOver = makeOnDragOver(elem, listeners.onDragOver)
-  var onDragLeave = makeOnDragLeave(elem, listeners.onDragLeave)
-  var onDrop = makeOnDrop(elem, listeners.onDrop, listeners.onDragLeave)
+  if (typeof listeners === 'function') {
+    listeners = { onDrop: listeners }
+  }
+
+  var timeout
 
   elem.addEventListener('dragenter', stopEvent, false)
   elem.addEventListener('dragover', onDragOver, false)
@@ -18,22 +21,14 @@ function dragDrop (elem, listeners) {
 
   // Function to remove drag-drop listeners
   return function remove () {
-    if (elem instanceof window.Element) elem.classList.remove('drag')
+    removeDragClass()
     elem.removeEventListener('dragenter', stopEvent, false)
     elem.removeEventListener('dragover', onDragOver, false)
     elem.removeEventListener('dragleave', onDragLeave, false)
     elem.removeEventListener('drop', onDrop, false)
   }
-}
 
-function stopEvent (e) {
-  e.stopPropagation()
-  e.preventDefault()
-  return false
-}
-
-function makeOnDragOver (elem, ondragover) {
-  return function (e) {
+  function onDragOver (e) {
     e.stopPropagation()
     e.preventDefault()
     if (e.dataTransfer.items) {
@@ -44,58 +39,93 @@ function makeOnDragOver (elem, ondragover) {
       if (items.length === 0) return
     }
 
-    if (elem instanceof window.Element) elem.classList.add('drag')
+    elem.classList.add('drag')
+    clearTimeout(timeout)
+
+    if (listeners.onDragOver) {
+      listeners.onDragOver(e)
+    }
+
     e.dataTransfer.dropEffect = 'copy'
-    if (ondragover) ondragover(e)
     return false
   }
-}
 
-function makeOnDragLeave (elem, ondragleave) {
-  return function (e) {
-    if (e.target !== elem) return
+  function onDragLeave (e) {
     e.stopPropagation()
     e.preventDefault()
-    if (ondragleave) ondragleave(e)
-    if (elem instanceof window.Element) elem.classList.remove('drag')
+
+    if (listeners.onDragLeave) {
+      listeners.onDragLeave(e)
+    }
+
+    clearTimeout(timeout)
+    timeout = setTimeout(removeDragClass, 50)
+
     return false
   }
-}
 
-function makeOnDrop (elem, ondrop, ondragleave) {
-  return function (e) {
+  function onDrop (e) {
     e.stopPropagation()
     e.preventDefault()
-    if (ondragleave) ondragleave(e)
-    if (elem instanceof window.Element) elem.classList.remove('drag')
-    var pos = { x: e.clientX, y: e.clientY }
+
+    if (listeners.onDragLeave) {
+      listeners.onDragLeave(e)
+    }
+
+    clearTimeout(timeout)
+    removeDragClass()
+
+    var pos = {
+      x: e.clientX,
+      y: e.clientY
+    }
+
     if (e.dataTransfer.items) {
       // Handle directories in Chrome using the proprietary FileSystem API
       var items = toArray(e.dataTransfer.items).filter(function (item) {
         return item.kind === 'file'
       })
+
       if (items.length === 0) return
+
       parallel(items.map(function (item) {
         return function (cb) {
           processEntry(item.webkitGetAsEntry(), cb)
         }
       }), function (err, results) {
-        // There should never be an error in production code. This catches permission
-        // errors with file:// in Chrome.
+        // This catches permission errors with file:// in Chrome. This should never
+        // throw in production code, so the user does not need to use try-catch.
         if (err) throw err
-        ondrop(flatten(results), pos)
+        if (listeners.onDrop) {
+          listeners.onDrop(flatten(results), pos)
+        }
       })
     } else {
       var files = toArray(e.dataTransfer.files)
+
       if (files.length === 0) return
+
       files.forEach(function (file) {
         file.fullPath = '/' + file.name
       })
-      ondrop(files, pos)
+
+      if (listeners.onDrop) {
+        listeners.onDrop(files, pos)
+      }
     }
 
     return false
   }
+
+  function removeDragClass () {
+    elem.classList.remove('drag')
+  }
+}
+
+function stopEvent (e) {
+  e.stopPropagation()
+  e.preventDefault()
+  return false
 }
 
 function processEntry (entry, cb) {
