bdc846fe4fdded00b0b638064d3be35fbef3d7d0
Feross Aboukhadijeh <feross@feross.org>
2020-10-28 18:46:07 -1000

    standard

--- buffer.js
+++ buffer.js
@@ -1,12 +1,12 @@
 module.exports = dragDropAsBuffer
 
-var dragDrop = require('./')
-var parallel = require('run-parallel')
-var blobToBuffer = require('blob-to-buffer')
+const dragDrop = require('./')
+const parallel = require('run-parallel')
+const blobToBuffer = require('blob-to-buffer')
 
 function dragDropAsBuffer (elem, cb) {
   return dragDrop(elem, function (files, pos, fileList) {
-    var tasks = files.map(function (file) {
+    const tasks = files.map(function (file) {
       return function (cb) {
         blobToBuffer(file, function (err, buffer) {
           if (err) return cb(err)
